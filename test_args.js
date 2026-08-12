4225ca8ec22e8fe0e767c0e003ccb658e9e42365
Ashwin Himanshu <ashwinhimanshu11@gmail.com>
2026-08-12 11:10:05 +0530

    chore: clean up leftover development and test scripts

--- test_args.js
+++
@@ -1,2 +0,0 @@
-const { exec } = require('child_process');
-exec(`python -c "import sys; print(sys.argv)" a b undefined`, (err, stdout) => console.log(stdout));
--- test_dummy.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import cv2
-img = np.zeros((100, 100, 3), dtype=np.uint8)
-cv2.imwrite("dummy.png", img)
